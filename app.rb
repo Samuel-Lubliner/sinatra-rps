19601d6f70ec6ef97303a36a62c11e4ff426aa4c
Samuel Lubliner <74507133+Samuel-Lubliner@users.noreply.github.com>
2023-08-04 20:42:35 +0000

    DRY for paper and scissors

--- app.rb
+++ app.rb
@@ -7,17 +7,25 @@ get("/") do
   erb(:homepage)
 end
 
-get("/rock") do
+get("/:move") do
   moves = ["rock", "paper", "scissors"]
+  @user_move = params[:move]
 
-  @comp_move = moves.sample
-  if @comp_move == "rock"
-    @outcome = "tied"
-  elsif @comp_move == "paper"
-    @outcome = "lost"
-  else
-    @outcome = "won"
+  unless moves.include?(@user_move)
+    redirect to("/")
   end
 
-  erb(:we_play_rock)
+  @comp_move = moves.sample
+
+  if @user_move == @comp_move
+    @outcome = "tied"
+  elsif (@user_move == "rock" && @comp_move == "scissors") ||
+         (@user_move == "paper" && @comp_move == "rock") ||
+         (@user_move == "scissors" && @comp_move == "paper")
+    @outcome = "won"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:we_play)
 end
